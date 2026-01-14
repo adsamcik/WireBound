9e5ee73c3ae368ac1ccec0618394f4ccfb072ffd
adsamcik <adsamcik@users.noreply.github.com>
2026-01-14 14:10:41 +0100

    refactor: update services to use WireBound.Core interfaces
    - Update LocalizationService to implement ILocalizationService from Core
    - Update NetworkPollingBackgroundService to use Core service interfaces

--- src/WireBound/Services/LocalizationService.cs
+++ src/WireBound/Services/LocalizationService.cs
@@ -1,33 +1,8 @@
 using System.Globalization;
 using System.Resources;
+using WireBound.Core.Services;
 
 namespace WireBound.Services;
-
-/// <summary>
-/// Service for retrieving localized strings from resource files.
-/// </summary>
-public interface ILocalizationService
-{
-    /// <summary>
-    /// Gets a localized string by its resource key.
-    /// </summary>
-    /// <param name="key">The resource key.</param>
-    /// <returns>The localized string, or the key if not found.</returns>
-    string GetString(string key);
-
-    /// <summary>
-    /// Gets a formatted localized string by its resource key.
-    /// </summary>
-    /// <param name="key">The resource key.</param>
-    /// <param name="args">Format arguments.</param>
-    /// <returns>The formatted localized string.</returns>
-    string GetString(string key, params object[] args);
-
-    /// <summary>
-    /// Gets the current culture being used for localization.
-    /// </summary>
-    CultureInfo CurrentCulture { get; }
-}
 
 /// <summary>
 /// Implementation of localization service using .resx resource files.
--- src/WireBound/Services/NetworkPollingBackgroundService.cs
+++ src/WireBound/Services/NetworkPollingBackgroundService.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WireBound.Core.Services;
 
 namespace WireBound.Services;
 
